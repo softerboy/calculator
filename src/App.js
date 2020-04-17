b7c272a267c0d7ae0af842e9f7524d2e559ec85c
softerboy <fn.umarov@gmail.com>
2020-04-17 22:46:42 +0500

    setup react bootstrap ui library

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import { Container, Row, Col, Alert } from 'react-bootstrap'
 
 function App() {
-  return <div>It&apos;s works</div>
+  return (
+    <Container className="mt-3">
+      <Row>
+        <Col>
+          <Alert variant="success">
+            The react bootstrap successfully setup
+          </Alert>
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
+import 'bootstrap/scss/bootstrap.scss'
+
 ReactDOM.render(
   <React.StrictMode>
     <App />

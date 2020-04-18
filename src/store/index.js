bee2f6a74c7d159e0ee035ce9635ca957238c416
softerboy <fn.umarov@gmail.com>
2020-04-18 21:55:24 +0500

    connect redux dev tools

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,10 @@ import { createStore, combineReducers } from 'redux'
 
 import * as reducers from './reducers'
 
-const store = createStore(combineReducers(reducers))
+const store = createStore(
+  combineReducers(reducers),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+)
 
 store.subscribe(function () {
   // eslint-disable-next-line no-console

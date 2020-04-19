bdc9777f19e8bcf5aaaca6cdaacc7fa11a73c74f
softerboy <fn.umarov@gmail.com>
2020-04-19 18:46:32 +0500

    handle -/+ sign button click

--- src/components/ButtonGridState.js
+++ src/components/ButtonGridState.js
@@ -8,7 +8,7 @@ import { SET_DISPLAY_RESULT } from '../store/action-types'
 
 export default function ButtonGridState() {
   const dispatch = useDispatch()
-  const { result } = useSelector(function (state) {
+  const { result: currentResult } = useSelector(function (state) {
     return state.display
   })
 
@@ -21,26 +21,32 @@ export default function ButtonGridState() {
   function onNumberButtonClick(target) {
     // do nothing if current displayed
     // input length max than MAX_INPUT_LENGTH constant
-    if (String(result).length >= MAX_INPUT_LENGTH) return
+    if (String(currentResult).length >= MAX_INPUT_LENGTH) return
 
     // in case of user pressed digit button and current
     // display input is integer, simply add selected digit
     // to back of current number
-    if (Number.isInteger(result)) {
-      const payload = { result: result * 10 + target.id }
+    if (Number.isInteger(currentResult)) {
+      const payload = { result: currentResult * 10 + target.id }
       return dispatch({ type: SET_DISPLAY_RESULT, payload })
     }
 
     // in case if user pressed digit button and current display
     // number is floating point, simply put entered digit to
     // back of current number
-    if (!Number.isInteger(result)) {
-      const payload = { result: result + '' + target.id }
+    if (!Number.isInteger(currentResult)) {
+      const payload = { result: currentResult + '' + target.id }
       return dispatch({ type: SET_DISPLAY_RESULT, payload })
     }
   }
 
   function onOperationButtonClick(target) {
+    if (target.id === buttons.BTN_SIGN && Number(currentResult) !== 0) {
+      // swap sign
+      const payload = { result: -1 * currentResult }
+      return dispatch({ type: SET_DISPLAY_RESULT, payload })
+    }
+
     // cancel last user entered input (number)
     // if CE (Cancel Entry) button pressed
     if (target.id === buttons.BTN_CANCEL_ENTRY) {
@@ -59,19 +65,22 @@ export default function ButtonGridState() {
     // We no need to add floating point twice if already exist in
     // current display input
     if (
-      String(result).indexOf('.') < 0 &&
+      String(currentResult).indexOf('.') < 0 &&
       target.id === buttons.BTN_FLOATING_POINT
     ) {
-      const payload = { result: result + '.' }
+      const payload = { result: currentResult + '.' }
       return dispatch({ type: SET_DISPLAY_RESULT, payload })
     }
 
     // if user entered Remove button simply remove last entered
     // digit or floating point from display
     if (target.id === buttons.BTN_REMOVE) {
-      const resultCharArray = String(result).split('')
+      const resultCharArray = String(currentResult).split('')
       const payload = {}
       if (resultCharArray.length === 1) {
+        payload.result = 0
+      } else if (resultCharArray.length === 2 && resultCharArray[0] === '-') {
+        // case minus (-) with one number
         payload.result = 0
       } else {
         payload.result = resultCharArray.slice(0, -1).join('')

f4d9d3174f9407486bbb186adc0ea10fa35586d6
softerboy <fn.umarov@gmail.com>
2020-04-23 05:18:50 +0500

    fix content slice error if expression type is number

--- src/core/calc.js
+++ src/core/calc.js
@@ -3,5 +3,5 @@ import MathCalc from '../lib/mathcalc'
 const math = new MathCalc()
 
 export default function calc(expression) {
-  return math.parse(expression).eval()
+  return math.parse(String(expression)).eval()
 }

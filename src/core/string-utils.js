94f73ea34423291a7f06310aa69bbae284bbff82
softerboy <fn.umarov@gmail.com>
2020-04-24 08:17:41 +0500

    fix scientific numbers issue with mathcalc.js

--- src/core/string-utils.js
+++ src/core/string-utils.js
@@ -19,3 +19,16 @@ function reverse(str) {
 export function unformat(input) {
   return String(input).replace(/,/g, '')
 }
+
+export function movePrecision(input) {
+  if (!(input.indexOf('.') > -1 && input.indexOf('e') > -1)) {
+    return input
+  }
+
+  const pointIndex = input.indexOf('.')
+  const indexOfE = input.indexOf('e')
+  const { length } = input.substring(pointIndex + 1, indexOfE)
+  const base = input.substring(0, indexOfE + 2)
+  const mantissa = input.substring(indexOfE + 1)
+  return base.replace('.', '') + Math.abs(Number(mantissa) - Number(length))
+}

74375b6ed9753c8b713dc0c56e961b622aef5195
softerboy <fn.umarov@gmail.com>
2020-04-27 01:50:19 +0500

    Update key bindings

--- src/common/keys.js
+++ src/common/keys.js
@@ -2,7 +2,7 @@ import { buttons, icons } from './constants'
 
 export default [
   [
-    { shortcut: 'p', symbol: icons.PERCENT, id: buttons.BTN_PERCENT },
+    { shortcut: ['%', 'p'], symbol: icons.PERCENT, id: buttons.BTN_PERCENT },
     { shortcut: 'c e', symbol: 'CE', id: buttons.BTN_CANCEL_ENTRY },
     { shortcut: 'c c', symbol: 'C', id: buttons.BTN_CLEAR },
     // eslint-disable-next-line
@@ -19,16 +19,16 @@ export default [
       symbol: icons.ONE_DIVIDE_BY,
       id: buttons.BTN_ONE_DIVIDE_BY,
     },
-    { shortcut: 's r', symbol: icons.SQUARE, id: buttons.BTN_SQUARE },
+    { shortcut: 's s', symbol: icons.SQUARE, id: buttons.BTN_SQUARE },
     // eslint-disable-next-line
-    { shortcut: 's t', symbol: icons.SQUARE_ROOT, id: buttons.BTN_SQUARE_ROOT },
-    { shortcut: 'd', symbol: icons.DIVIDE, id: buttons.BTN_DIVIDE },
+    { shortcut: 's r', symbol: icons.SQUARE_ROOT, id: buttons.BTN_SQUARE_ROOT },
+    { shortcut: ['/', 'd'], symbol: icons.DIVIDE, id: buttons.BTN_DIVIDE },
   ],
   [
     { shortcut: '7', symbol: '7', id: buttons.BTN_NUM_SEVEN },
     { shortcut: '8', symbol: '8', id: buttons.BTN_NUM_EIGHT },
     { shortcut: '9', symbol: '9', id: buttons.BTN_NUM_NINE },
-    { shortcut: 'm', symbol: icons.MULTIPLY, id: buttons.BTN_MULTIPLY },
+    { shortcut: ['*', 'm'], symbol: icons.MULTIPLY, id: buttons.BTN_MULTIPLY },
   ],
   [
     { shortcut: '4', symbol: '4', id: buttons.BTN_NUM_FOUR },
@@ -40,7 +40,7 @@ export default [
     { shortcut: '1', symbol: '1', id: buttons.BTN_NUM_ONE },
     { shortcut: '2', symbol: '2', id: buttons.BTN_NUM_TWO },
     { shortcut: '3', symbol: '3', id: buttons.BTN_NUM_THREE },
-    { shortcut: ['a', '+'], symbol: icons.ADD, id: buttons.BTN_ADD },
+    { shortcut: ['+', 'a'], symbol: icons.ADD, id: buttons.BTN_ADD },
   ],
   [
     { shortcut: 's g', symbol: icons.SIGN, id: buttons.BTN_SIGN },
